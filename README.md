4b6b4975eb39c4d5f06a614a811f754618e038e5
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-03-22 16:53:06 -0300

    refact

--- README.md
+++ README.md
@@ -1,51 +1,26 @@
-# Flutter Firebase Push
+# Flutter Firebase Push Notifications
 
-A simple Flutter App connected to Firebase that receives messages.
+A simple Flutter App connected to Firebase that sends and receives messages.
 
-## Getting Started
+<img src=".docs/android.png" height=400/>
 
-Create Firebase project and update app config:
+### Running the app
 
-1. Access [Firebase Console](https://console.firebase.google.com) and create a new project.
-2. In the "Cloud Messaging" blade select to add an "Android" app.
-3. Give it a package name `com.example.flutter_azure_push` (must match `AndroidManifest.xml`).
-4. Register the app.
-5. Download and copy the `google-services.json` file to the Android `app` directoy.
-6. Add dependencies *(not needed for this project, already set up)*.
-
-## Running the app
+You'll need the Flutter SDK and FlutterFire SDK dependencies locally.
 
 After the setup, run the app:
 
-1. Connect your device.
-2. `flutter packages get`
-3. `flutter run`
+```sh
+flutter pub get
+flutter run
+```
 
-## Test in Firebase
+To lear how to setup your own Firebase project and interact with it check the [FlutterFire docs](https://firebase.flutter.dev/docs/overview).
 
-1. Copy the device token from the console output.
-2. Go to the Firebase Console and select "Send your first message".
-3. Set title and body texts.
-4. Create test message, and add the device token.
-5. Select the App.
-6. Review and publish message.
+### Sources
 
-Your Flutter App should be updated with the message received.
-
-## Azure Notification Hub
-
-Possibility to send push via Azure Notification Hub.
-
-1. Creat a resource group and the notification hub ([Template](https://azure.microsoft.com/en-us/resources/templates/101-notification-hub/)).
-2. Copy the `Server Key` from the Firebase Console.
-3. Paste into **Google (GCM/FCM)** blade **API Key** field.
-4. Test/Send via the Portal option
-
-
-## References
-
+```
 https://firebase.google.com/docs/android/setup?authuser=0
-
 https://pub.dev/packages/firebase_messaging
-
 https://github.com/flutter/plugins/tree/master/packages/firebase_messaging
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Flutter Firebase Push'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
-  String message = 'Firebase message will show up here';
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  String _receivedPushMessage = "...";
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  // It is assumed that all messages contain a data field with the key 'type'
+  Future<void> setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    // If the message also contains a data property with a "type" of "chat",
+    // navigate to a chat screen
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    // Also handle any interaction when the app is in the background via a
+    // Stream listener
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _listenForMessages() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+        setState(() {
+          String? body = message.notification?.body;
+          if (body != null) {
+            this._receivedPushMessage = body;
+          } else {
+            this._receivedPushMessage = "message boy was null";
+          }
+        });
+      }
+    });
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    if (message.data['type'] == 'chat') {
+      Navigator.pushNamed(
+        context,
+        '/chat',
+        arguments: message,
+      );
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print('on message $message');
-        _setMessageText(message.toString());
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print('on resume $message');
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print('on launch $message');
-      },
+
+    // Run code required to handle interacted messages in an async function
+    // as initState() must not be async
+    _requestPermission();
+    // setupInteractedMessage();
+    _listenForMessages();
+  }
+
+  void _requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
     );
 
-    _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+      print(await FirebaseMessaging.instance.getToken());
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
 
-    _firebaseMessaging.getToken().then((token) {
-      print(token);
+  /// The API endpoint here accepts a raw FCM payload for demonstration purposes.
+  String constructFCMPayload(String? token) {
+    return jsonEncode({
+      'token': token,
+      'data': {'via': 'FlutterFire Cloud Messaging!!!'},
+      'notification': {
+        'title': 'Hello FlutterFire!',
+        'body': 'This notification was created via FCM!',
+      },
     });
   }
 
-  _setMessageText(String text) {
-    setState(() {
-      message = text;
-    });
-  }
+  Future<void> _sendPush() async {
+    // myController.text;
 
-  _resetText() {
-    _setMessageText('Firebase message will show up here');
-  }
+    var _token = await FirebaseMessaging.instance.getToken();
 
-  _bodyWidget() {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              message,
-              style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
-            ),
-          ],
-        ),
-      ),
-    );
+    if (_token == null) {
+      print('Unable to send FCM message, no token exists.');
+      return;
+    }
+
+    try {
+      await http.post(
+        // Reusing this URL from the FlutterFire examples
+        Uri.parse('https://api.rnfirebase.io/messaging/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: constructFCMPayload(_token),
+      );
+      print('FCM request for device sent!');
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      body: _bodyWidget(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Received Push will show here:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: Text(
+                _receivedPushMessage,
+                style: const TextStyle(
+                    fontSize: 32,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 100),
+            Text(
+              'Type your message and send a push:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                style: const TextStyle(fontSize: 32),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your push message',
+                ),
+                controller: myController,
+              ),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          _resetText();
-        },
+        onPressed: _sendPush,
+        tooltip: 'Send Push',
+        child: const Icon(Icons.send),
       ),
     );
   }
